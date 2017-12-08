7a46f564f8e65d49c15dee873224f1c2c8e02821
ManiKarnika <dscwortsov@gmail.com>
2017-12-07 22:45:54 -0800

    запуск браузеров

--- tests/test_hw_2_3_login_script_esr.py
+++ tests/test_hw_2_3_login_script_esr.py
@@ -2,12 +2,14 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from time import sleep
 
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Firefox()
+    wd = webdriver.Firefox(capabilities={"marionette": False},
+                           firefox_binary="/Applications/tools/tools_firefox/esr/FirefoxESR.app/Contents/MacOS/firefox-bin")
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
